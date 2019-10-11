eb9340f5d313418c69eab2c1bc53b09363e2998a
Kaliszewski <kaliszewskipawel@o2.pl>
2019-10-11 21:00:12 +0200

    simplyfy class Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,11 @@
-package src.java;
-
 import java.io.IOException;
 
 class Main {
 
     public static void main(String[] args) {
 
-        menuSearch menu = new Menu();
-        manager fileManager = new FileManager();
-
         try {
-            fileManager.createFile();
-            menu.searchMenu();
+            new Menu().searchMenu();
         }
         catch (IOException e) {
             System.out.println(e.getMessage());
